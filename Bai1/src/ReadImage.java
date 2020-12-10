27609055d0ee629719eb33293f9584b9ec8ee52d
vanhieu0410 <65800833+vanhieu0410@users.noreply.github.com>
2020-12-11 01:37:51 +0700

    Add files via upload

--- Bai1/src/ReadImage.java
+++ Bai1/src/ReadImage.java
@@ -40,6 +40,7 @@ public class ReadImage {
 		int width = img.getWidth();
 		int height = img.getHeight();
 		
+		
 		for(int y = 0; y < height; y++) {
 			for(int x =0; x <width; x++) {
 				int p = img.getRGB(x, y);
@@ -48,8 +49,15 @@ public class ReadImage {
 				int r = (p>>16)&0xff;
 				int g = (p>>8)&0xff;
 				int b = p&0xff;
+				int tmp = 0;
+				if (r>b && r>2*g ) {
+					tmp = b;
+					b = r;
+					r = tmp;
+					
+				}
 				
-				p = (a<<24) | 0 | (g<<8) | b; 
+				p = (a<<24) | (r<<16) | (g<<8) | b; 
 				img.setRGB(x, y, p);
 			}
 		}
